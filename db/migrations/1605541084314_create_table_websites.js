c399458b15bedbbb53f1ebd24acced5a96feecd2
farazahmad759 <farazahmad759@gmail.com>
2020-11-16 21:55:03 +0500

    working on generating api functions

---
+++ db/migrations/1605541084314_create_table_websites.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+    return knex.schema.createTable('websites', function (table) {
+      table.increments();
+table.string('_url').notNullable();
+table.string('_status').nullable();
+
+    });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable('websites');
+  };
+  
--- node-scripts/crud-api-functions.js
+++ node-scripts/crud-api-functions.js
@@ -1,39 +1,26 @@
 import fs from 'fs';
 export function generateContent(jsonData) {
-  let content = `
+  let c_imports = `
   import nextConnect from 'next-connect';
   import { knex } from '../../../../knexfile';
-  const handler = nextConnect();
-  handler.post(async (req, res) => {
+  const handler = nextConnect();`;
+  let c_post = `handler.post(async (req, res) => {
     let data = req.body;
-    let categories = await knex('categories').insert({
+    let resCategories = await knex('${jsonData.tableName}').insert({
       ...req.body,
     });
     res.json({
       message: 'Record created successfully',
-      data: categories,
+      data: resCategories,
     });
-  });
-  
-  handler.get(async (req, res) => {
-    if (!req.user) {
-      res.json({
-        message: 'Unauthorized',
-      });
-    }
-    res.statusCode = 200;
-    let ret = {
-      _title: 'Entertainment',
-      _description: 'Entertainment category',
-    };
-    ret = await knex.select().from('categories');
-    res.json({ user: req.user, data: ret });
-  });
-  
-  export default handler;
-  
-  `;
-  return content;
+  });`;
+  let c_get = `handler.get(async (req, res) => {
+    let resCategories = await knex.select().from('${jsonData.tableName}');
+    res.json({ message: 'Following record(s) have been found', data: resCategories });
+  });`;
+  let c_exports = `export default handler;`;
+  let c_return = c_imports + c_post + c_get + c_exports;
+  return c_return;
 }
 
 export const generateFile = (tableName, content) => {
