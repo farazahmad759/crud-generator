5c3b286f83658132215a03e207662eb642b59892
farazahmad759 <farazahmad759@gmail.com>
2020-11-16 23:16:23 +0500

    createFile function has been improved

--- node-scripts/functions.js
+++ node-scripts/functions.js
@@ -2,9 +2,14 @@ import fs from 'fs';
 /**
  * createFile(object)
  */
-const dvCreateFile = (params) => {
-  let { name, content, dir, preName, postName, extension } = params;
+const createFile = (params) => {
+  let { name, type, content, dir, preName, postName, extension } = params;
   if (!name) {
+    console.error('params.name is required');
+    return false;
+  }
+  if (!type) {
+    console.error('params.type is required');
     return false;
   }
   if (!content) {
@@ -26,9 +31,18 @@ const dvCreateFile = (params) => {
   if (!fs.existsSync(dir)) {
     fs.mkdirSync(dir);
   }
-  let formattedName =
-    dir + new Date().getTime() + '_create_table_' + name + extension;
-  fs.writeFile(formattedName, content, function (err) {
+  let formattedName = '';
+  if (type === 'migration') {
+  }
+  formattedName += preName + '_' + name + '_' + postName + extension;
+  formattedName = formattedName.replace('__', '_');
+  formattedName = formattedName.replace('__', '_');
+  formattedName = formattedName.replace('__', '_');
+  formattedName = formattedName.replace('__', '_');
+  if (formattedName.charAt(0) === '_') {
+    formattedName = formattedName.substring(1);
+  }
+  fs.writeFile(dir + formattedName, content, function (err) {
     if (err) {
       console.log(err);
     } else {
@@ -38,9 +52,10 @@ const dvCreateFile = (params) => {
 };
 
 /**
- * dvReadJsonFromFile
+ * readSchema(object)
+ * @returns jsonObject
  */
-const dvReadSchemaFile = (params) => {
+const readSchema = (params) => {
   let { name, dir } = params;
   if (!name) {
     return false;
@@ -48,10 +63,10 @@ const dvReadSchemaFile = (params) => {
   if (!dir) {
     dir = 'db/schemas/';
   }
-  let rawdata = fs.readFileSync(file);
+  let rawdata = fs.readFileSync(dir + name);
   let jsonData = JSON.parse(rawdata);
   return jsonData;
 };
-let dvFunctions = { dvCreateFile, dvReadSchemaFile };
+let dvFunctions = { createFile, readSchema };
 
 export default dvFunctions;
--- node-scripts/index.js
+++ node-scripts/index.js
@@ -1,7 +1,17 @@
 import { generateFile, generateContent } from './migration-functions.js';
 import fs from 'fs';
 import dvFunctions from './functions.js';
-dvFunctions.dvCreateFile({ name: 'new', dir: 'db/migrations/' });
+import dvMigrations from './migrations/migrations.js';
+let usersJson = dvFunctions.readSchema({ name: 'users.json' });
+console.log(usersJson);
+let usersMigration = dvMigrations.buildContent({ jsonData: usersJson });
+dvFunctions.createFile({
+  name: 'users',
+  type: 'migration',
+  dir: 'db/migrations/',
+  preName: new Date().getTime() + '_create_table',
+  postName: '',
+});
 // const schemaDirectory = 'dv-crud/schemas';
 
 // fs.readdirSync(schemaDirectory).forEach((file) => {
